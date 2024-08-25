a65112b8a9dfefe9bdbea6efc4d552c278b4f5ba
Neha Sethi <learn.nehasethi@gmail.com>
2024-08-25 15:04:12 +0530

    Time: 4 ms (72.66%), Space: 41.6 MB (76.42%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
@@ -5,13 +5,9 @@ class Solution {
         for(int i = 0 ; i < mid ; i++)
         {
             int j = s.length() - 1 - i;
-            if(((s.charAt(i) == 'a') || (s.charAt(i) == 'A') || (s.charAt(i) == 'e')
-            ||(s.charAt(i) == 'E') || (s.charAt(i) == 'i') || (s.charAt(i) == 'I') || (s.charAt(i) ==
-            'o') || (s.charAt(i) == 'O') ||(s.charAt(i) == 'u') || (s.charAt(i) == 'U')))
+            if("aeiouAEIOU".indexOf(s.charAt(i)) != -1)
                 a++;
-            if(((s.charAt(j) == 'a') || (s.charAt(j) == 'A') || (s.charAt(j) == 'e')
-            ||(s.charAt(j) == 'E') || (s.charAt(j) == 'i') || (s.charAt(j) == 'I') || (s.charAt(j) ==
-            'o') || (s.charAt(j) == 'O') ||(s.charAt(j) == 'u') || (s.charAt(j) == 'U')))
+            if("aeiouAEIOU".indexOf(s.charAt(j)) != -1)
                 b++;
         }
         if(a == b){
