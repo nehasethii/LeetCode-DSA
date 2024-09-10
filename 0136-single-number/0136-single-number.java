84a7848be066b465cafa787fc87df6603d51f4a4
Neha Sethi <learn.nehasethi@gmail.com>
2024-09-11 05:07:39 +0530

    Time: 0 ms (100%), Space: 46.4 MB (11.28%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if(nums.length == 1)
-            return nums[0];
-        Arrays.sort(nums);
-        for(int i = 0 ; i < nums.length; i++)
-        {
-            if(i == nums.length - 1)
-                return nums[i];
-            if(nums[i] == nums[i + 1])
-                i++;
-            else
-                return nums[i];    
-        }
-        return 0;
+        return xor(0,nums);
+    }
+    public static int xor(int i , int[] nums)
+    {
+        if(i >= nums.length)
+            return 0;
+        return nums[i] ^ xor(i+1,nums);
     }
 }
