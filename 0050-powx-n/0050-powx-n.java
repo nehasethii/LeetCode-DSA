251270fed6035ff71b3a5553d1c0f7b30ca4adc5
Neha Sethi <learn.nehasethi@gmail.com>
2024-09-12 05:36:52 +0530

    Time: 0 ms (100%), Space: 41.7 MB (83.62%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return pow(x, (long) n);
+    }
+    public static double pow(double x , long n)
+    {
+        if(n == 0)
+            return 1;
+        if(n < 0)
+            return 1.0/pow(x,-n);
+        if(n % 2 != 0)
+            return x * pow(x * x , (n - 1)/2);
+        else
+            return pow(x * x , n / 2);
+    }
+}
