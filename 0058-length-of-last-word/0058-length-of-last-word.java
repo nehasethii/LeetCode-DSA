5c8c0d2b7dd0b72186358bfc10e9b07c3fc33b20
Neha Sethi <learn.nehasethi@gmail.com>
2024-09-14 05:13:21 +0530

    Time: 1 ms (32.84%), Space: 42.1 MB (6.45%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,5 +1,5 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        return (((s.split(" "))[s.split(" ").length - 1]).length());
+    return (s.split(" "))[s.split(" ").length - 1].length();
     }
 }
