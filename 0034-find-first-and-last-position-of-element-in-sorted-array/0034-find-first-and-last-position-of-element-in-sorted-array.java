aa4adcf47396e4578ba1c8c221743ecf2264560f
Neha Sethi <learn.nehasethi@gmail.com>
2024-12-18 16:04:15 +0530

    Time: 0 ms (100%), Space: 46 MB (28.71%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,31 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = -1, last = -1;
-        first = binarySearch(nums,target,true);
-        if(first != -1)
-            last = binarySearch(nums,target,false);
-        return new int[]{first,last};
+        int startIdx = binarySearch(nums,target,true);
+        if(startIdx == -1)
+            return new int[]{-1,-1};
+        else
+        {
+            int endIdx = binarySearch(nums,target,false);
+            return new int[]{startIdx,endIdx};
+        }
     }
-    public int binarySearch(int[] nums,int target,boolean isStartIdx)
+    int binarySearch(int[] arr , int target , boolean firstAppearance)
     {
-        int start = 0;
-        int end = nums.length - 1;
         int idx = -1;
+        int start = 0;
+        int end = arr.length - 1;
         while(start <= end)
         {
             int mid = start + (end - start)/2;
-            if(target > nums[mid])
-                start = mid + 1;
-            else if(target < nums[mid])
+            if(target < arr[mid])
                 end = mid - 1;
+            else if(target > arr[mid])
+                start = mid + 1;
             else
             {
-                idx = mid;
-                if(isStartIdx)
+                if(firstAppearance)
+                {
+                    idx = mid;
                     end = mid - 1;
+                }
                 else
+                {
+                    idx = mid;
                     start = mid + 1;
-            }
+                }
+            }   
         }
         return idx;
     }
