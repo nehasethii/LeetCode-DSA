26bf0a723befce254782ede1a839045941ecec3e
Neha Sethi <learn.nehasethi@gmail.com>
2024-12-18 20:48:43 +0530

    Time: 0 ms (100%), Space: 43.2 MB (17.92%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -5,11 +5,11 @@ class Solution {
         while(start <= end)
         {
             int mid = start + (end - start)/2;
-            if(target < nums[mid])
-                end = mid - 1;
-            else if(target > nums[mid])
+            if(target > nums[mid])
                 start = mid + 1;
-            else 
+            else if(target < nums[mid])
+                end = mid - 1;
+            else
                 return mid;
         }
         return start;
