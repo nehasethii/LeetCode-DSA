c32f25a26b4a74631fa3e892277b81493dcb9b51
Neha Sethi <learn.nehasethi@gmail.com>
2024-12-19 16:14:46 +0530

    Time: 0 ms (100%), Space: 43.1 MB (64.23%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -2,26 +2,25 @@ class Solution {
     public boolean search(int[] nums, int target) {
         int pivot = findPivot(nums);
         if(pivot == -1)
-            return (binarySearch(nums,target,0,nums.length - 1) != -1);
-        if(nums[pivot] == target)
+            return binarySearch(nums,target,0,nums.length - 1);
+        if(target == nums[pivot])
             return true;
         if(target >= nums[0])
-            return (binarySearch(nums,target,0,pivot - 1) != -1);
-        return (binarySearch(nums,target,pivot + 1,nums.length - 1) != -1);
+            return binarySearch(nums,target,0,pivot-1);
+        return binarySearch(nums,target,pivot + 1,nums.length - 1);
     }
-    static int findPivot(int[] nums)
+    int findPivot(int[] nums)
     {
-        int pivot;
         int start = 0;
         int end = nums.length - 1;
         while(start <= end)
         {
             int mid = start + (end - start)/2;
-            if((mid < end) && (nums[mid] > nums[mid + 1]))
+            if(mid < end && nums[mid] > nums[mid + 1])
                 return mid;
-            if((mid > start) && (nums[mid] < nums[mid - 1]))
-                return (mid - 1);
-            if(nums[start] == nums[mid] && nums[end] == nums[mid])
+            if(mid > start && nums[mid] < nums[mid - 1])
+                return mid - 1;
+            if((nums[mid] == nums[start]) && (nums[mid] == nums[end]))
             {
                 if(start < nums.length - 1 && nums[start] > nums[start + 1])
                     return start;
@@ -30,14 +29,15 @@ class Solution {
                     return end - 1;
                 end--;
             }
-            else if((nums[start] < nums[mid]) || (nums[start] == nums[mid] && nums[mid] > nums[end]))
+            else if(nums[mid] > nums[start] || (nums[mid] == nums[start] && 
+            nums[mid] > nums[end]))
                 start = mid + 1;
             else
                 end = mid - 1;
         }
         return -1;
     }
-    static int binarySearch(int[] nums,int target, int start , int end)
+    boolean binarySearch(int[] nums ,int target,int start,int end)
     {
         while(start <= end)
         {
@@ -47,8 +47,8 @@ class Solution {
             else if(target < nums[mid])
                 end = mid - 1;
             else
-                return mid;
+                return true;
         }
-        return -1;
+        return false;
     }
 }
