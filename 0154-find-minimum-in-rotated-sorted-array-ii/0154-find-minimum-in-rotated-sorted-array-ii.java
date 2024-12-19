cc917745e97ab8e890142faa84e075088a8ca563
Neha Sethi <learn.nehasethi@gmail.com>
2024-12-19 16:46:37 +0530

    Time: 0 ms (100%), Space: 43.6 MB (8.04%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -1,32 +1,33 @@
 class Solution {
     public int findMin(int[] nums) {
         int pivot = findPivot(nums);
-        return (pivot == -1 || pivot == nums.length - 1) ? nums[0] : nums[pivot + 1];
+        return (pivot == -1) ? nums[0] : nums[pivot + 1];
     }
     int findPivot(int[] nums)
     {
         int start = 0;
         int end = nums.length - 1;
-        while(start < end)
+        while(start <= end)
         {
             int mid = start + (end - start)/2;
-            if(mid < end && nums[mid] > nums[mid + 1])
-                return mid;
             if(mid > start && nums[mid] < nums[mid - 1])
                 return mid - 1;
-            if(nums[start] == nums[mid] && nums[end] == nums[mid])
+            if(mid < end && nums[mid] > nums[mid + 1])
+                return mid;
+            if(nums[mid] == nums[start] && nums[mid] == nums[end])
             {
-                if(nums[start] > nums[start + 1])
+                if(start < nums.length - 1 && nums[start] > nums[start + 1])
                     return start;
                 start++;
-                if(nums[end] < nums[end - 1])
-                    return end;
+                if(end > 0 && nums[end] < nums[end-1])
+                    return end - 1;
                 end--;
             }
-            else if(nums[start] > nums[mid] || (nums[start] == nums[mid] && nums[end] > nums[mid]))
+            else if(nums[mid] < nums[start] || 
+            (nums[mid] == nums[start] && nums[mid] < nums[end]))
                 end = mid - 1;
             else
-                start = mid + 1; 
+                start = mid + 1;
         }
         return -1;
     }
