8135ec7e5f9ead7363c621f49c7b3bc11d885fd8
Neha Sethi <learn.nehasethi@gmail.com>
2024-12-31 09:09:55 +0530

    Time: 0 ms (100%), Space: 42.6 MB (85.62%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.java
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.java
@@ -1,35 +1,12 @@
-import java.util.Scanner;
 class Solution {
-    public static void main(String[] args)
-    {
-        Scanner in = new Scanner(System.in);
-        int m = in.nextInt();
-        int n = in.nextInt();
-        int[][] accounts = new int[m][n];
-        for(int i = 0 ; i < m ; i++)
-        {
-            for(int j = 0 ; j < n ; j++)
-            {
-                accounts[i][j] = in.nextInt();
-            }
-        }
-        Solution sol = new Solution();
-        int max = sol.maximumWealth(accounts);
-        System.out.println(max);
-    }
     public int maximumWealth(int[][] accounts) {
-        int max = 0 , sum;
-        for(int i = 0 ; i < accounts.length ; i++)
+        int max = 0;
+        for(int[] acc : accounts)
         {
-            sum = 0;
-            for(int j = 0 ; j < accounts[i].length ; j++)
-            {
-                sum += accounts[i][j];
-            }
-            if(sum > max)
-            {
-                max = sum;
-            }
+            int wealth = 0;
+            for(int money : acc)
+                wealth += money;
+            max = Math.max(max,wealth);
         }
         return max;
     }
