fa12fc3133921b0a4dced7c9df839bb5d24fff8a
Neha Sethi <learn.nehasethi@gmail.com>
2024-12-31 09:07:19 +0530

    Time: 0 ms (100%), Space: 42.8 MB (30.45%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.java
@@ -1,33 +1,7 @@
-import java.util.Scanner;
-
 class Solution {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Solution sol = new Solution();
-        int n = in.nextInt();
-        int[] nums = new int[n];
-        for (int i = 0; i < nums.length; i++)
-        {
-            nums[i] = in.nextInt();
-        }
-        int[] runningSum = sol.runningSum(nums);
-        for (int i = 0; i < nums.length; i++)
-        {
-            System.out.print(runningSum[i] + " ");
-        }
-    }
     public int[] runningSum(int[] nums) {
-        int[] runningSum = new int[nums.length];
-        int i , j;
-        for (i = 0; i < nums.length; i++)
-        {
-            int sum = 0;
-            for (j = 0; j <= i; j++)
-            {
-                sum += nums[j];
-            }
-            runningSum[j-1] = sum;
-        }
-        return runningSum;
+        for(int i = 1 ; i < nums.length ; i++)
+            nums[i] += nums[i-1];
+        return nums;
     }
 }
