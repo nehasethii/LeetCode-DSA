dc1cd52eec69aa8d6ec16c8694790e720f81559c
Neha Sethi <learn.nehasethi@gmail.com>
2025-01-18 20:08:34 +0530

    Time: 1 ms (97.85%), Space: 45.5 MB (24.86%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,25 +1,8 @@
-import java.util.Scanner;
 class Solution {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Solution sol = new Solution();
-        int[] nums = new int[6];
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-            nums[i] = in.nextInt();
-        }
-        int[] resultantArray = sol.buildArray(nums);
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-            System.out.print(resultantArray[i] + " ");
-        }
-    }
     public int[] buildArray(int[] nums) {
-        int[] resultantArray = new int[nums.length];
-        for(int i = 0 ; i < resultantArray.length ; i++)
-        {
-            resultantArray[i] = nums[nums[i]];
-        }
-        return resultantArray;
+        int[] ans = new int[nums.length];
+        for(int i = 0 ; i < nums.length ; i++)
+            ans[i] = nums[nums[i]];
+        return ans;
     }
 }
