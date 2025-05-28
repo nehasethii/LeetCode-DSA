c6805860e534f14172539a2c879dbf7395ac85f1
Neha Sethi <learn.nehasethi@gmail.com>
2025-05-28 18:07:14 +0530

    Time: 1 ms (83.57%), Space: 41.3 MB (25.44%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,25 +1,53 @@
 class Solution {
     public int reverse(int x) {
-        String str = Integer.toString(x);
-        StringBuilder sb = new StringBuilder(str);
         int orgNum = x;
-        int reverseNum = 0 , remainder = 0;
+        int revNum = 0;
         if(x < 0)
             x = -x;
-        if ( x > 0 ){
-            while(x > 0)
-            {
-                if(reverseNum > Integer.MAX_VALUE/10) 
-                {
-                    return 0;
-                } 
-                remainder = x % 10;
-                reverseNum = reverseNum * 10 + remainder;
-                x /= 10;
-            }
-        }  
+        while(x > 0){
+            int rem = x % 10;
+            int temp = revNum * 10 + rem;
+            if(revNum > ((int)Math.pow(2,31) - 1 - rem )/10)
+                return 0;
+            else
+                revNum = temp;
+            x /= 10;
+        }
         if(orgNum < 0)
-            return -reverseNum;
-        return reverseNum;
+            revNum = -revNum;
+        return revNum;
+
+
+
+
+
+
+
+
+
+
+
+
+        // String str = Integer.toString(x);
+        // StringBuilder sb = new StringBuilder(str);
+        // int orgNum = x;
+        // int reverseNum = 0 , remainder = 0;
+        // if(x < 0)
+        //     x = -x;
+        // if ( x > 0 ){
+        //     while(x > 0)
+        //     {
+        //         if(reverseNum > Integer.MAX_VALUE/10) 
+        //         {
+        //             return 0;
+        //         } 
+        //         remainder = x % 10;
+        //         reverseNum = reverseNum * 10 + remainder;
+        //         x /= 10;
+        //     }
+        // }  
+        // if(orgNum < 0)
+        //     return -reverseNum;
+        // return reverseNum;
     }
 }
