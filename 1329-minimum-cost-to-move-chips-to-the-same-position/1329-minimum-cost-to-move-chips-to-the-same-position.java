f98cec7745731b28c4319e2e8091dccc82b3ac3f
Neha Sethi <learn.nehasethi@gmail.com>
2025-06-01 13:01:47 +0530

    Time: 0 ms (100%), Space: 41.1 MB (68.82%) - LeetHub

--- 1329-minimum-cost-to-move-chips-to-the-same-position/1329-minimum-cost-to-move-chips-to-the-same-position.java
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/1329-minimum-cost-to-move-chips-to-the-same-position.java
@@ -1,13 +1,12 @@
 class Solution {
     public int minCostToMoveChips(int[] position) {
-        int evenPositions = 0 , oddPositions = 0;
-        for(int i = 0 ; i < position.length ; i++ )
-        {
-            if(position[i] % 2 == 0) evenPositions++;
-            else oddPositions++;
-        }
-        if(evenPositions == position.length || oddPositions == position.length) 
-            return 0;
-        return Math.min(evenPositions,oddPositions);
+       int oddPos = 0 , evenPos = 0;
+       for(int i = 0 ; i < position.length ; i++){
+        if(position[i] % 2 == 0)
+            evenPos++;
+        else
+            oddPos++;
+       }
+       return (evenPos > oddPos) ? oddPos : evenPos;
     }
 }
