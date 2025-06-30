1203a3d8b42596ac52523b8b3a9c07c0a59ec8a8
Neha Sethi <learn.nehasethi@gmail.com>
2025-06-30 06:22:11 +0530

    Time: 3 ms (98.37%), Space: 44.6 MB (40.67%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,71 +1,18 @@
 class Solution {
     public String intToRoman(int num) {
-        StringBuilder roman = new StringBuilder();
-        while(num >= 1000)
-        {
-            roman.append("M");
-            num -= 1000;
+        int[] value = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        String[] symbol = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        StringBuilder sb = new StringBuilder();
+        int index = 12;
+        while(num > 0){
+            if(num >= value[index]){
+                sb.append(symbol[index]);
+                num -= value[index];
+            }
+            else{
+                index--;
+            }
         }
-        if(num >= 900)
-        {
-            roman.append("CM");
-            num -= 900;
-        }
-        while(num >= 500)
-        {
-            roman.append("D");
-            num -= 500;
-        }
-        if(num >= 400)
-        {
-            roman.append("CD");
-            num -= 400;
-        }
-        while(num >= 100)
-        {
-            roman.append("C");
-            num -= 100;
-        }
-        if(num >= 90)
-        {
-            roman.append("XC");
-            num -= 90;
-        }
-        while(num >= 50)
-        {
-            roman.append("L");
-            num -= 50;
-        }
-        if(num >= 40)
-        {
-            roman.append("XL");
-            num -= 40;
-        }
-        while(num >= 10)
-        {
-            roman.append("X");
-            num -= 10;
-        }
-        if(num >= 9)
-        {
-            roman.append("IX");
-            num -= 9;
-        }
-        while(num >= 5)
-        {
-            roman.append("V");
-            num -= 5;
-        }
-        if(num >= 4)
-        {
-            roman.append("IV");
-            num -= 4;
-        }
-        while(num >= 1)
-        {
-            roman.append("I");
-            num -= 1;
-        }
-        return roman.toString();
+        return sb.toString();
     }
 }
