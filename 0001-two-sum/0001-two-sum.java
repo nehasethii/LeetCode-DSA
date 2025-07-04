80eb5770763ff79c20f291fe397c8dff9f33b844
Neha Sethi <learn.nehasethi@gmail.com>
2025-07-05 05:22:59 +0530

    0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -10,28 +10,5 @@ class Solution {
             }
             sol[0]++;
         }
-
-
-
-
-
-
-
-
-
-
-        // int[] solution = new int[2];
-        // for(int i = 0 ; i < nums.length - 1 ; i++)
-        // {
-        //     for(int j = nums.length - 1 ; j != i ; j--)
-        //     {
-        //         if((nums[i] + nums[j]) == target)
-        //         {
-        //             solution[0] = i;
-        //             solution[1] = j;
-        //         }
-        //     }
-        // }  
-        // return solution;      
     }
 }
