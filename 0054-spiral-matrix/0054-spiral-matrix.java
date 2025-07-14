8e90e096d8d6b2053fb841f83b3b5dc22c971b7c
Neha Sethi <learn.nehasethi@gmail.com>
2025-07-14 05:39:21 +0530

    Time: 0 ms (100%), Space: 41.7 MB (26.58%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,27 +1,31 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> spiralMatrix = new ArrayList<Integer>();
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int top = 0, left = 0, right = cols - 1, bottom = rows - 1;
-        while (top <= bottom && left <= right) {
-            for (int i = left; i <= right; i++)
-                spiralMatrix.add(matrix[top][i]);
+        int n = matrix.length , m = matrix[0].length;
+        int top = 0 , left = 0;
+        int bottom = n - 1 , right = m - 1;
+        List<Integer> spiral = new ArrayList<>();
+        while(spiral.size() < m * n){
+            for(int i = left ; i <= right ; i++){
+                spiral.add(matrix[top][i]);
+            }
             top++;
-            for (int j = top; j <= bottom; j++)
-                spiralMatrix.add(matrix[j][right]);
+            for(int i = top ; i <= bottom ; i++){
+                spiral.add(matrix[i][right]);
+            }
             right--;
-            if (top <= bottom) {
-                for (int k = right; k >= left; k--)
-                    spiralMatrix.add(matrix[bottom][k]);
-                bottom--;
+            if(top > bottom)
+                break;
+            for(int i = right ; i >= left ; i--){
+                spiral.add(matrix[bottom][i]);
             }
-            if (left <= right) {
-                for (int l = bottom; l >= top; l--)
-                    spiralMatrix.add(matrix[l][left]);
-                left++;
+            bottom--;
+            if(left > right)
+                break;
+            for(int i = bottom ; i >= top ; i--){
+                spiral.add(matrix[i][left]);
             }
+            left++;
         }
-        return spiralMatrix;
+        return spiral;
     }
 }
