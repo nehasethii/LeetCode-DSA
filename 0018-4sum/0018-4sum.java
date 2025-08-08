5b2c01baacb8ebb4b13f61577317da30b331be23
Neha Sethi <learn.nehasethi@gmail.com>
2025-08-08 08:23:43 +0530

    Time: 15 ms (88.43%), Space: 44 MB (47.04%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,8 +1,8 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n = nums.length;
         Arrays.sort(nums);
-        List<List<Integer>> sol = new ArrayList<>();
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
         for(int i = 0 ; i < n ; i++){
             if(i > 0 && nums[i] == nums[i-1]){
                 continue;
@@ -14,12 +14,12 @@ class Solution {
                 int k = j + 1;
                 int l = n - 1;
                 while(k < l){
-                    long tgt = (long)target - nums[i] - nums[j] - nums[k];
-                    if(nums[l] > tgt){
-                        l--;
-                    }
-                    else if(nums[l] < tgt){
+                    long sum = (long)nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum < target){
                         k++;
+                    }
+                    else if(sum > target){
+                        l--;
                     }
                     else{
                         List<Integer> temp = new ArrayList<>();
@@ -27,19 +27,19 @@ class Solution {
                         temp.add(nums[j]);
                         temp.add(nums[k]);
                         temp.add(nums[l]);
-                        sol.add(temp);
+                        ans.add(temp);
                         k++;
                         l--;
                         while(k < n && nums[k] == nums[k-1]){
                             k++;
                         }
-                        while(l > 0 && nums[l] == nums[i+1]){
+                        while(l > 0 && nums[l] == nums[l+1]){
                             l--;
                         }
                     }
                 }
             }
         }
-        return sol;
+        return ans;
     }
 }
