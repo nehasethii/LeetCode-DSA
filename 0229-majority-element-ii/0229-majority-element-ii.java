fa7b58589bc1bbc769d3699e1a5ea1f255cf77b4
Neha Sethi <learn.nehasethi@gmail.com>
2025-08-08 09:56:03 +0530

    Time: 3 ms (76.76%), Space: 50.7 MB (16.76%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,49 +1,44 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> major = new ArrayList<>();
         int n = nums.length;
-        int ele1 = -1 , ele2 = -1;
-        int cnt1 = 0 , cnt2 = 0;
-        for(int i = 0 ; i < n ; i++)
-        {
-            if(nums[i] == ele1)
-                cnt1++;
-            else if(nums[i] == ele2)
-                cnt2++;
-            else if(cnt1 == 0)
-            {
-                ele1 = nums[i];
-                cnt1 = 1;
+        int c1 = 0 , c2 = 0 , v1 = 0 , v2 = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(v1 > 0 && c1 == nums[i]){
+                v1++;
             }
-            else if(cnt2 == 0)
-            {
-                ele2 = nums[i];
-                cnt2 = 1;
+            else if(v2 > 0 && c2 == nums[i]){
+                v2++;
             }
-            else
-            {
-                cnt1--;
-                cnt2--;
+            else if(v1 == 0){
+                c1 = nums[i];
+                v1++;
+            }
+            else if(v2 == 0){
+                c2 = nums[i];
+                v2++;
+            }
+            else{
+                v1--;
+                v2--;
             }
         }
-        cnt1 = cnt2 = 0;
-        for(int el : nums)
-        {
-            if(el == ele1)
-                cnt1++;
-            if(el == ele2)
-                cnt2++;
+        v1 = 0;
+        v2 = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] == c1){
+                v1++;
+            }
+            if(nums[i] == c2){
+                v2++;
+            }
         }
-        if(cnt1 > Math.floor(n/3))
-            major.add(ele1);
-        if(cnt2 > Math.floor(n/3) && ele1 != ele2)
-            major.add(ele2);
-        if(major.size() == 2 && major.get(0) > major.get(1))
-        {
-            int temp = major.get(0);
-            major.set(0,major.get(1));
-            major.set(1,temp);
+        List<Integer> ans = new ArrayList();
+        if(v1 > n/3){
+            ans.add(c1);
         }
-        return major;
+        if(c1 != c2 && v2 > n/3){
+            ans.add(c2);
+        }
+        return ans;
     }
 }
