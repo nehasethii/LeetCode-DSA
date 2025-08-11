de95b0461914df3555c1dec7056cd7950447705d
Neha Sethi <learn.nehasethi@gmail.com>
2025-08-11 12:08:50 +0530

    Time: 2 ms (79.87%), Space: 46.5 MB (23.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,20 +3,15 @@ class Solution {
         int z = -1;
         for(int i = 0 ; i < nums.length ; i++){
             if(nums[i] == 0){
-                if(z == -1)
+                if(z == -1){
                     z = i;
-            }
-            else if(z == -1){
-                continue;
+                }
             }
             else{
-                nums[z] = nums[i];
-                nums[i] = 0;
-                if(i - z > 1){
+                if(z != -1){
+                    nums[z] = nums[i];
+                    nums[i] = 0;
                     z++;
-                }
-                else{
-                    z = i;
                 }
             }
         }
