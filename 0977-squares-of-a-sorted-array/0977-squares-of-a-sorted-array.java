0ee3d1eb8dcecc2f5b7979eb5966dfe114ec37b2
Neha Sethi <learn.nehasethi@gmail.com>
2026-01-21 17:47:13 +0530

    Time: 1 ms (99.86%), Space: 47.6 MB (33.96%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,40 +1,21 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int idx1 = -1;
-        for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i] <= 0){
-                idx1 = i;
+        int n = nums.length;
+        for(int i = 0 ; i < n ; i++){
+            nums[i] = nums[i] * nums[i];
+        }
+        int i = 0;
+        int j = n - 1;
+        int[] ans = new int[n];
+        int idx = n-1;
+        while(i <= j){
+            if(nums[i] > nums[j]){
+                ans[idx--] = nums[i++];
+            }
+            else{
+                ans[idx--] = nums[j--];
             }
         }
-        if(idx1 < 0){
-            for(int i = 0 ; i < nums.length ; i++){
-                nums[i] = nums[i] * nums[i];
-            }
-            return nums;
-        }
-        else{
-            int idx2 = idx1 + 1;
-            int j = 0;
-            int[] ans = new int[nums.length];
-            while(idx1 >= 0 && idx2 < nums.length){
-                if(nums[idx1] * nums[idx1] < nums[idx2] * nums[idx2]){
-                    ans[j++] = nums[idx1] * nums[idx1];
-                    idx1--;
-                }
-                else{
-                    ans[j++] = nums[idx2] * nums[idx2];
-                    idx2++;
-                }
-            }
-            while(idx1 >= 0){
-                ans[j++] = nums[idx1] * nums[idx1];
-                idx1--;
-            }
-            while(idx2 < nums.length){
-                ans[j++] = nums[idx2] * nums[idx2];
-                idx2++;
-            }
-            return ans;
-        }
+        return ans;
     }
 }
