e1a44b07f6d85032a662c1d111a85d1cb6479a74
Neha Sethi <learn.nehasethi@gmail.com>
2026-03-07 01:03:32 +0530

    Time: 0 ms (100%), Space: 48.4 MB (27.91%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,37 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int startIdx = binarySearch(nums,target,true);
-        if(startIdx == -1)
-            return new int[]{-1,-1};
-        else
-        {
-            int endIdx = binarySearch(nums,target,false);
-            return new int[]{startIdx,endIdx};
+        int[] ans = new int[2];
+        ans[0] = BS(nums,target,true);
+        if(ans[0] == -1){
+            ans[1] = -1;
         }
+        else{
+            ans[1] = BS(nums,target,false);
+        }
+        return ans;
     }
-    int binarySearch(int[] arr , int target , boolean firstAppearance)
-    {
-        int idx = -1;
-        int start = 0;
-        int end = arr.length - 1;
-        while(start <= end)
-        {
-            int mid = start + (end - start)/2;
-            if(target < arr[mid])
-                end = mid - 1;
-            else if(target > arr[mid])
-                start = mid + 1;
-            else
-            {
-                if(firstAppearance)
-                {
-                    idx = mid;
-                    end = mid - 1;
+    public int BS(int[] nums , int target , boolean first){
+        int low = 0;
+        int high = nums.length - 1;
+        int res = -1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(target < nums[mid]){
+                high = mid - 1;
+            }
+            else if(target > nums[mid]){
+                low = mid + 1;
+            }
+            else{
+                res = mid;
+                if(first){
+                    high = mid - 1;
                 }
-                else
-                {
-                    idx = mid;
-                    start = mid + 1;
+                else{
+                    low = mid + 1;
                 }
-            }   
+            }
         }
-        return idx;
+        return res;
     }
 }
