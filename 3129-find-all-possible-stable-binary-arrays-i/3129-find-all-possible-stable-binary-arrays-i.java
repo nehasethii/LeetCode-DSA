f7e064ffe3651bcb1f46bc796e9d9a6a7fa2d375
Neha Sethi <learn.nehasethi@gmail.com>
2026-03-09 21:34:36 +0530

    Time: 1023 ms (6.58%), Space: 277.5 MB (6.58%) - LeetHub

---
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.java
@@ -0,0 +1,47 @@
+class Solution {
+    int mod = 1000000007;
+    int[][][][] dp;
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        dp = new int[zero+1][one+1][2][limit+1];
+        for(int i=0;i<=zero;i++){
+            for(int j=0;j<=one;j++){
+                for(int k=0;k<2;k++){
+                    Arrays.fill(dp[i][j][k],-1);
+                }
+            }
+        }
+        long ans = 0;
+        if(zero > 0)
+            ans += countStableArrays(zero-1, one, 0, 1, limit);
+        if(one > 0)
+            ans += countStableArrays(zero, one-1, 1, 1, limit);
+        return (int)(ans % mod);
+    }
+    public int countStableArrays(int z, int o, int last, int count , int limit){
+        if(z == 0 && o == 0){
+            return 1;
+        }
+        if(dp[z][o][last][count] != -1){
+            return dp[z][o][last][count];
+        }
+        long ans = 0;
+        if(z > 0){
+            if(last != 0){
+                ans += countStableArrays(z-1,o,0,1,limit);
+            }
+            else if(count < limit){
+                ans += countStableArrays(z-1,o,0,count+1,limit);
+            }
+        }
+        if(o > 0){
+            if(last != 1){
+                ans += countStableArrays(z,o-1,1,1,limit);
+            }
+            else if(count < limit){
+                ans += countStableArrays(z,o-1,1,count+1,limit);
+            }
+        }
+        dp[z][o][last][count] = (int)(ans % mod);
+        return dp[z][o][last][count];
+    }
+}
