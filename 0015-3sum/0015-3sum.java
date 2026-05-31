d455bae40df0027a857358998c169e6c9816568f
Neha Sethi <learn.nehasethi@gmail.com>
2026-05-31 07:41:47 +0530

    Time: 33 ms (73.68%), Space: 60.7 MB (5.59%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,38 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> triplets = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 0 ; i < n - 2 ; i++){
             if(i > 0 && nums[i] == nums[i-1]){
                 continue;
             }
-            int j = i + 1 , k = n - 1;
-            int target = -nums[i];
-            while(j < k){
-                List<Integer> temp = new ArrayList<>();
-                if(nums[j] + nums[k] < target){
-                    j++;
-                }
-                else if(nums[j] + nums[k] > target){
-                    k--;
-                }
-                else{
+            int j = i+1 , k = n - 1;
+            while(j < n && k < n && j < k){
+                if((nums[j] + nums[k]) == -nums[i]){
+                    List<Integer> temp = new ArrayList<>();
                     temp.add(nums[i]);
                     temp.add(nums[j]);
                     temp.add(nums[k]);
-                    ans.add(temp);
+                    triplets.add(temp);
                     j++;
                     k--;
                     while(j < n && nums[j] == nums[j-1]){
                         j++;
                     }
-                    while(k > 0 && nums[k] == nums[k+1]){
-                        k--;
+                    while(k > n && nums[k] == nums[k+1]){
+                        k++;
                     }
+                }
+                else if(nums[j] + nums[k] < -nums[i]){
+                    j++;
+                }
+                else{
+                    k--;
                 }
             }
         }
-        return ans;
+        return triplets;
     }
 }
