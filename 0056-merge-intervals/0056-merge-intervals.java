e073f95f3c6e204e4e8e3e31c5aaf57a9bd5132b
Neha Sethi <learn.nehasethi@gmail.com>
2026-07-02 20:47:01 +0530

    Time: 9 ms (35.63%), Space: 48.7 MB (91.71%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,28 +1,37 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0 ; i < intervals.length ; i++){
-            int s = intervals[i][0];
-            int e = intervals[i][1];
-            int n = ans.size();
-            if(n == 0 || s > ans.get(n-1).get(1)){
-                List<Integer> temp = new ArrayList<>();
-                temp.add(s);
-                temp.add(e);
-                ans.add(temp);
+        Arrays.sort(intervals , (a,b) -> a[0] - b[0]);
+        List<List<Integer>> list = new ArrayList<>();
+        int start1 = intervals[0][0];
+        int end1 = intervals[0][1];
+        for(int i = 1 ; i < intervals.length ; i++){
+            int start2 = intervals[i][0];
+            int end2 = intervals[i][1];
+            if(end1 >= start2){ //merge
+                start1 = start1;
+                end1 = Math.max(end1 , end2);
+                continue;
             }
-            else{
-                e = Math.max(e,ans.get(n-1).get(1));
-                ans.get(n-1).set(1,e); 
-            }
+            List<Integer> temp = new ArrayList<>();
+            temp.add(start1);
+            temp.add(end1);
+            list.add(temp);
+            start1 = start2;
+            end1 = end2;
         }
-        int n = ans.size();
-        int[][] sol = new int[n][2];
+        List<Integer> temp = new ArrayList<>();
+        temp.add(start1);
+        temp.add(end1);
+        list.add(temp);
+
+        int n = list.size();
+        int m = list.get(0).size();
+        int[][] ans = new int[n][m];
         for(int i = 0 ; i < n ; i++){
-            sol[i][0] = ans.get(i).get(0);
-            sol[i][1] = ans.get(i).get(1);
-        }
-        return sol;
+            for(int j = 0 ; j < m ; j++){
+                ans[i][j] = list.get(i).get(j);
+            }
+        } 
+        return ans;
     }
 }
