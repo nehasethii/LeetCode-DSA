364027ef62a5e45ef8af412347faf6bc569ea796
Neha Sethi <learn.nehasethi@gmail.com>
2026-07-04 18:01:50 +0530

    Time: 4 ms (8.98%), Space: 46.8 MB (88.72%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,29 +1,38 @@
 class Solution {
-    public int[][] insert(int[][] interval, int[] newInterval) {
-        List<List<Integer>> intervals = new ArrayList<>();
-        for(int i = 0 ; i < interval.length ; i++){
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<List<Integer>> intervalsList = new ArrayList<>();
+        boolean inserted = false;
+        for(int i = 0 ; i < intervals.length ; i++){
+            int start = intervals[i][0];
+            if(!inserted && start >= newInterval[0]){
+                List<Integer> temp = new ArrayList<>();
+                temp.add(newInterval[0]);
+                temp.add(newInterval[1]);
+                intervalsList.add(temp);
+                inserted = true;
+            }
             List<Integer> temp = new ArrayList<>();
-            temp.add(interval[i][0]);
-            temp.add(interval[i][1]);
-            intervals.add(temp);
+            temp.add(intervals[i][0]);
+            temp.add(intervals[i][1]);
+            intervalsList.add(temp);
         }
-        List<Integer> temp = new ArrayList<>();
-        temp.add(newInterval[0]);
-        temp.add(newInterval[1]);
-        intervals.add(temp);
-        return merge(intervals);
+        if(!inserted){
+            List<Integer> temp = new ArrayList<>();
+            temp.add(newInterval[0]);
+            temp.add(newInterval[1]);
+            intervalsList.add(temp);
+        }
+        return merge(intervalsList);
     }
-    public int[][] merge(List<List<Integer>> intervals) {
-        Collections.sort(intervals , (a,b) -> a.get(0) - b.get(0));
+    public int[][] merge(List<List<Integer>> intervals){
         List<List<Integer>> list = new ArrayList<>();
         int start1 = intervals.get(0).get(0);
         int end1 = intervals.get(0).get(1);
         for(int i = 1 ; i < intervals.size() ; i++){
             int start2 = intervals.get(i).get(0);
             int end2 = intervals.get(i).get(1);
-            if(end1 >= start2){ //merge
-                start1 = start1;
-                end1 = Math.max(end1 , end2);
+            if(end1 >= start2){
+                end1 = Math.max(end1,end2);
                 continue;
             }
             List<Integer> temp = new ArrayList<>();
@@ -31,20 +40,17 @@ class Solution {
             temp.add(end1);
             list.add(temp);
             start1 = start2;
-            end1 = end2;
+            end1 = end2; 
         }
         List<Integer> temp = new ArrayList<>();
         temp.add(start1);
         temp.add(end1);
         list.add(temp);
-
-        int n = list.size();
-        int m = list.get(0).size();
-        int[][] ans = new int[n][m];
-        for(int i = 0 ; i < n ; i++){
+        int[][] ans = new int[list.size()][2];
+        for(int i = 0 ; i < list.size() ; i++){
             ans[i][0] = list.get(i).get(0);
             ans[i][1] = list.get(i).get(1);
-        } 
+        }
         return ans;
     }
 }
