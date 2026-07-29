7349d040de3f6b92370502576d5827ad67858666
Neha Sethi <learn.nehasethi@gmail.com>
2026-07-29 23:04:19 +0530

    Time: 19 ms (38.99%), Space: 82.9 MB (12.9%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,43 +1,38 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
         int n = bloomDay.length;
-        if((long)m * k > n){
-            return -1;
-        }
-        int ans = 0;
-        int low = 1;
-        int high = 0;
+        long low = 0;
+        long high = 0;
         for(int day : bloomDay){
+            low = Math.min(low,day);
             high = Math.max(high,day);
         }
+        int ans = -1;
         while(low <= high){
-            int mid = low + (high - low)/2;
-            int adj = 0;
-            int bouquet = 0;
+            long mid = low + (high - low)/2;
+            int flowers = 0;
+            int bouquets = 0;
             for(int i = 0 ; i < n ; i++){
-                int day = bloomDay[i];
-                if(day <= mid){
-                    if(i > 0 && bloomDay[i-1] <= mid){
-                        adj++;
-                    }
-                    else{
-                        adj = 1;
-                    }
-                    if(adj == k){
-                        bouquet++;
-                        adj = 0;
-                    }
-                    if(bouquet >= m){
-                        break;
-                    }
+                if(bloomDay[i] <= mid){
+                    flowers++;
+                }
+                else{
+                    flowers = 0;
+                }
+                if(flowers == k){
+                    bouquets++;
+                    flowers = 0;
+                }
+                if(bouquets >= m){
+                    break;
                 }
             }
-            if(bouquet >= m){
-                ans = mid;
-                high = mid - 1;
+            if(bouquets < m){
+                low = mid + 1;
             }
             else{
-                low = mid + 1;
+                ans = (int)mid;
+                high = mid - 1;
             }
         }
         return ans;
