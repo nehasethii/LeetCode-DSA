cc65b6cf03d4102fe8d76426f35f6415b22a6084
Neha Sethi <learn.nehasethi@gmail.com>
2026-07-31 22:39:52 +0530

    Time: 10 ms (2.08%), Space: 45.7 MB (28.98%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,25 +1,24 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums){
-        List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
         List<Integer> temp = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
-        permutations(nums,ans,temp,visited);
-        return ans;
+        List<List<Integer>> ans = new ArrayList<>();
+        return permutations(nums,temp,ans);
     }
-    public void permutations(int[] nums,List<List<Integer>> ans, List<Integer> temp, boolean[] visited){
+    public List<List<Integer>> permutations(int[] nums, List<Integer> temp , List<List<Integer>> ans){
         if(temp.size() == nums.length){
-            List<Integer> res = new ArrayList<>(temp);
-            ans.add(res);
-            return;
-        }
-        for(int i = 0 ; i < nums.length ; i++){
-            if(!visited[i]){
-                temp.add(nums[i]);
-                visited[i] = true;
-                permutations(nums,ans,temp,visited);
-                temp.remove(temp.size() - 1);
-                visited[i] = false;
+            if(!ans.contains(temp)){
+                List<Integer> permutation = new ArrayList<>(temp);
+                ans.add(permutation);
+                return ans;
             }
         }
+        for(int i = 0 ; i < nums.length ; i++){
+            if(!temp.contains(nums[i])){
+                temp.add(nums[i]);
+                permutations(nums,temp,ans);
+                temp.remove(temp.size() - 1);
+            }
+        }
+        return ans;
     }
 }
