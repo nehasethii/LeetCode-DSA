87ae1bb17c65abb3a9d5485503e23cd0186cef97
Neha Sethi <learn.nehasethi@gmail.com>
2026-08-09 19:27:05 +0530

    Time: 8 ms (16%), Space: 45.1 MB (68.29%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,27 +1,35 @@
 class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        int n = candidates.length;
         Arrays.sort(candidates);
-        List<List<Integer>> ans = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
-        generateCombinations(candidates,target,ans,temp,0,0);
+        boolean[] visited = new boolean[n];
+        generateCombinations(candidates,target,0,temp,visited,0);
         return ans;
     }
-    public void generateCombinations(int[] candidates, int target, List<List<Integer>> ans, List<Integer> temp, int sum, int start){
+    public void generateCombinations(int[] candidates, int target, int sum, List<Integer> temp, boolean[] visited , int idx){
         if(sum > target){
             return;
         }
         if(sum == target){
-            List<Integer> res = new ArrayList<>(temp);
-            ans.add(res);
+            ans.add(new ArrayList<>(temp));
             return;
         }
-        for(int i = start ; i < candidates.length ; i++){
-            if(i > start && candidates[i] == candidates[i-1]){
+        for(int i = 0 ; i < candidates.length ; i++){
+            if(i < idx){
                 continue;
             }
-            temp.add(candidates[i]);
-            generateCombinations(candidates,target,ans,temp,sum+candidates[i],i+1);
-            temp.remove(temp.size() - 1);
+            if(i > 0 && candidates[i] == candidates[i-1] && !visited[i-1]){
+                continue;
+            }
+            if(!visited[i]){
+                visited[i] = true;
+                temp.add(candidates[i]);
+                generateCombinations(candidates, target, sum + candidates[i], temp, visited, i);
+                temp.remove(temp.size() - 1);
+                visited[i] = false;
+            }
         }
     }
 }
