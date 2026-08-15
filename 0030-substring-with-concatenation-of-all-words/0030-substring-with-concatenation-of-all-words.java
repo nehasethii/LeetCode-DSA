94f0a8ed2432ac6283c54858e7ba1631bdde491d
Neha Sethi <learn.nehasethi@gmail.com>
2026-08-15 18:29:26 +0530

    Time: 21 ms (7.55%), Space: 49 MB (5.61%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -1,52 +1,46 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        //Declarations
+        int skip = words[0].length();
         List<Integer> ans = new ArrayList<>();
-        Map<String,Integer> map1 = new HashMap<>(); //needed
-        Map<String,Integer> map2 = new HashMap<>();  //available
-        int n = s.length();
-        int w = words.length;
-        int wl = words[0].length();
-        //int size = w * wl;
-        int count = 0;
-
-        //needed mapping
-        for(int i = 0 ; i < w ; i++){
-            map1.put(words[i] , map1.getOrDefault(words[i],0) + 1);
+        Map<String,Integer> need = new HashMap<>();
+        Map<String,Integer> have = new HashMap<>();
+        String str = "";
+        int size = words.length * words[0].length();
+        
+        for(int i = 0 ; i < words.length ; i++){
+            need.put(words[i],need.getOrDefault(words[i],0) + 1);
         }
+        for(int i = 0 ; i < words[0].length() ; i++){
+            int low = i;
+            int high = i;
+            have.clear();
+            str = "";
+            for(high = i ; high + skip <= s.length() ; high += skip){
+                String temp = s.substring(high , high + skip);
 
-        int low = 0 , high = 0;
-        for(int start = 0 ; start < wl ; start++){//for offsets 0,3,6... 1,4,7... 2,5,8...
-            map2.clear();
-            count = 0;
-            low = start;
-            high = start;
-            for( ; high + wl <= n ; high += wl){
-                // include high
-                String str = s.substring(high , high + wl);
-                if(!map1.containsKey(str)){
-                    map2.clear();
-                    count = 0;
-                    low = high + wl;
+                if(!need.containsKey(temp)){
+                    have.clear();
+                    str = "";
+                    low = high + skip;
                     continue;
-                }
-                map2.put(str,map2.getOrDefault(str,0) + 1);
-                count++;
+                }    
 
-                // while incorrect
-                while(map2.get(str) > map1.get(str)){
-                    String leftStr = s.substring(low, low + wl);
-                    map2.put(leftStr,map2.get(leftStr) - 1);
-                    if(map2.get(leftStr) == 0){
-                        map2.remove(leftStr);
-                    }
-                    low += wl;
-                    count--;
+                str += temp;
+                have.put(temp,have.getOrDefault(temp,0) + 1);
+
+                while(have.getOrDefault(temp,0) > need.getOrDefault(temp,0)){
+                    String t = s.substring(low , low + skip);
+                    have.put(t,have.getOrDefault(t,0) - 1);
+                    str = str.substring(skip,str.length());
+                    low += skip;
                 }
 
-                //if correct
-                if(count == w){
+                if(str.length() == size){
                     ans.add(low);
+                    temp = s.substring(low , low + skip);
+                    have.put(temp,have.getOrDefault(temp,0) - 1);
+                    low += skip;
+                    str = s.substring(low,high + skip);
                 }
             }
         }
